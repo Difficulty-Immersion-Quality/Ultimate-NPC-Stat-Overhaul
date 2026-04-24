f3f89d6c4fcc4ea7364818cde174c51b8915b1c7
BaldursGoonsack <149293305+BaldursGoonsack@users.noreply.github.com>
2026-04-25 03:16:43 +1000

    Only _Subclasses.lua changed, need to overhaul _Core.lua

---
+++ UNSO - Lab/Ultimate NPC Stat Overhaul - Lab Edition/Mods/ScriptExtender/Lua/Server/_Subclasses.lua
@@ -0,0 +1,132 @@
+ClassData = {
+    Barbarian = {
+        MainPassive = "LAB_Progression_Barbarian",
+        SubclassTable = {
+            "LAB_Progression_Barbarian_Berserker",
+            "LAB_Progression_Barbarian_Giant",
+            "LAB_Progression_Barbarian_WildMagic",
+            "LAB_Progression_Barbarian_Wildheart",
+        },
+        SubclassUnlockLevel = 3
+    },
+    Bard = {
+        MainPassive = "LAB_Progression_Bard",
+        SubclassTable = {
+            "LAB_Progression_Bard_Glamour",
+            "LAB_Progression_Bard_Lore",
+            "LAB_Progression_Bard_Swords",
+            "LAB_Progression_Bard_Valor"
+        },
+        SubclassUnlockLevel = 3
+    },
+    Cleric = {
+        MainPassive = "LAB_Progression_Cleric",
+        SubclassTable = {
+            "LAB_Progression_Cleric_Death",
+            "LAB_Progression_Cleric_Life",
+            "LAB_Progression_Cleric_Light",
+            "LAB_Progression_Cleric_Knowledge",
+            "LAB_Progression_Cleric_Nature",
+            "LAB_Progression_Cleric_Tempest",
+            "LAB_Progression_Cleric_Trickery",
+            "LAB_Progression_Cleric_War"
+        },
+        SubclassUnlockLevel = 1
+    },
+    Druid = {
+        MainPassive = "LAB_Progression_Druid",
+        SubclassTable = {
+            "LAB_Progression_Druid_Land",
+            "LAB_Progression_Druid_Moon",
+            "LAB_Progression_Druid_Spores",
+            "LAB_Progression_Druid_Stars"
+        },
+        SubclassUnlockLevel = 2
+    },
+    Fighter = {
+        MainPassive = "LAB_Progression_Fighter",
+        SubclassTable = {
+            "LAB_Progression_Fighter_ArcaneArcher",
+            "LAB_Progression_Fighter_BattleMaster",
+            "LAB_Progression_Fighter_Champion",
+            "LAB_Progression_Fighter_EldritchKnight"
+        },
+        SubclassUnlockLevel = 3
+    },
+    Monk = {
+        MainPassive = "LAB_Progression_Monk",
+        SubclassTable = {
+            "LAB_Progression_Monk_DrunkenMaster",
+            "LAB_Progression_Monk_FourElements",
+            "LAB_Progression_Monk_OpenHand",
+            "LAB_Progression_Monk_Shadow"
+        },
+        SubclassUnlockLevel = 3
+    },
+    Paladin = {
+        MainPassive = "LAB_Progression_Paladin",
+        SubclassTable = {
+            "LAB_Progression_Paladin_Ancients",
+            "LAB_Progression_Paladin_Crown",
+            "LAB_Progression_Paladin_Devotion",
+            "LAB_Progression_Paladin_Vengeance",
+            "LAB_Progression_Paladin_Oathbreaker"
+        },
+        SubclassUnlockLevel = 1
+    },
+    Ranger = {
+        MainPassive = "LAB_Progression_Ranger",
+        SubclassTable = {
+            "LAB_Progression_Ranger_BeastMaster",
+            "LAB_Progression_Ranger_GloomStalker",
+            "LAB_Progression_Ranger_Hunter",
+            "LAB_Progression_Ranger_Swarmkeeper"
+        },
+        SubclassUnlockLevel = 3
+    },
+    Rogue = {
+        MainPassive = "LAB_Progression_Rogue",
+        SubclassTable = {
+            "LAB_Progression_Rogue_ArcaneTrickster",
+            "LAB_Progression_Rogue_Assassin",
+            "LAB_Progression_Rogue_Swashbuckler",
+            "LAB_Progression_Rogue_Thief"
+        },
+        SubclassUnlockLevel = 3
+    },
+    Sorcerer = {
+        MainPassive = "LAB_Progression_Sorcerer",
+        SubclassTable = {
+            "LAB_Progression_Sorcerer_DraconicBloodline",
+            "LAB_Progression_Sorcerer_WildMagic",
+            "LAB_Progression_Sorcerer_Shadow",
+            "LAB_Progression_Sorcerer_StormSorcery"
+        },
+        SubclassUnlockLevel = 1
+    },
+    Warlock = {
+        MainPassive = "LAB_Progression_Warlock",
+        SubclassTable = {
+            "LAB_Progression_Warlock_Archfey",
+            "LAB_Progression_Warlock_Fiend",
+            "LAB_Progression_Warlock_GreatOldOne",
+            "LAB_Progression_Warlock_Hexblade"
+        },
+        SubclassUnlockLevel = 1
+    },
+    Wizard = {
+        MainPassive = "LAB_Progression_Wizard",
+        SubclassTable = {
+            "LAB_Progression_Wizard_Abjuration",
+            "LAB_Progression_Wizard_Bladesinging",
+            "LAB_Progression_Wizard_Conjuration",
+            "LAB_Progression_Wizard_Divination",
+            "LAB_Progression_Wizard_Enchantment",
+            "LAB_Progression_Wizard_Evocation",
+            "LAB_Progression_Wizard_Illusion",
+            "LAB_Progression_Wizard_Necromancy",
+            "LAB_Progression_Wizard_Transmutation"
+        },
+        SubclassUnlockLevel = 2
+    }
+}
